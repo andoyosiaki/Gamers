c5cdc65ed9357107d1d14bd628e30c8a28093b3c
andou <yutori4dayo@gmail.com>
2019-08-04 13:33:00 +0900

    finish

--- logout.php
+++ logout.php
@@ -4,6 +4,7 @@ ini_set('display_errors',1);
 
 
 $_SESSION = array();
+
 if(ini_get("session.use_cookies")){
   $params = session_get_cookie_params();
   setcookie(session_name(),'',time()-4200,$params["path"],$params["domain"],$params["secure"],$params["httponly"]);
