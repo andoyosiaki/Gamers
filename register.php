3040b3f7c29f98d639823b0f12f4a0e3b91a9fcc
andou <yutori4dayo@gmail.com>
2019-08-04 12:44:18 +0900

    add display error

--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 session_start();
 require_once(__DIR__.'/core/dbconect.php');
 require(__DIR__.'/functions/functions.php');
+ini_set('display_errors',1);
 
 if(!empty($_POST)){
   if($_POST['name'] === ''){
